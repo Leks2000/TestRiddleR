3c6b162f93a2457a8f69a54b0ff3f364db2b6030
Leks2000 <alex.halle06@list.ru>
2023-03-30 12:07:48 +0300

    firststep

--- app/src/main/java/com/example/testriddler/Antwort.kt
+++ app/src/main/java/com/example/testriddler/Antwort.kt
@@ -4,12 +4,39 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.RadioButton
+import android.widget.RadioGroup
 
 class Antwort : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.antwort)
+        lateinit var intentResult: Intent
+        intentResult = Intent(this, MainActivity::class.java)
+        val btnantw: Button = findViewById(R.id.btnAnt)
+
+
+        val rdbtns = arrayOf("ant1", "ant2", "ant3","ant4","ant5","ant6","ant7","ant8","ant9","ant10")
+        val main = findViewById(R.id.linelay) as LinearLayout
+        val rg = RadioGroup(this)
+        rg.orientation = RadioGroup.VERTICAL
+        for (i in rdbtns.indices) {
+            val rb = RadioButton(this)
+            rb.text = antworts.random()
+            rb.id = View.generateViewId()
+            rb.setOnClickListener{
+                intentResult.putExtra("Result", rb.text);
+                btnantw.isEnabled = true
+            }
+            rg.addView(rb)
+        }
+        main.addView(rg)
     }
+
+    val antworts = listOf("Водопровод", "Граммофон", "Календарь", "Крапива", "Машины", "Сапоги", "Магнитофон", "Таракан", "Щетка", "Муравейник", "Наперсток", "Муха", "Паутина", "ладно текст.", "Троллейбус.")
+
     fun tomain(view: View) {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
--- app/src/main/java/com/example/testriddler/MainActivity.kt
+++ app/src/main/java/com/example/testriddler/MainActivity.kt
@@ -30,23 +30,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         riddle = findViewById(R.id.secatct)
+        activ = findViewById(R.id.secondActivity)
+        answer = findViewById(R.id.yourAnswer3)
         riddleQuestions = findViewById(R.id.riddleQuestions)
     }
+
+    val riddles = listOf("Речка спятила с ума - По домам пошла сама.","Музыкант, певец, рассказчик — А всего труба да ящик.","На раскрашенных страницах Много праздников хранится.",
+        "Ах, не трогайте меня: Обожгу и без огня!","Маленькие домики по улице бегут, Мальчиков и девочек домики везут.",
+        "Пустые отдыхаем, А полные шагаем.","Нет ушей, а слышит. Нету рук, а пишет.","Над нами кверху ногами.","Деревянный брусок, А на нем растет лесок.",
+        "В лесу без огня котел кипит.","На ямке, ямке сто ямок с ямкой.","Кто над нами вверх ногами?","В углу сито, не руками вито.",
+        "Было 4 овцы как","Что это такое: синий, большой, с усами и набит зайцами?")
+
     private lateinit var riddle: TextView
+    private lateinit var activ: TextView
+    private lateinit var answer: TextView
     private lateinit var riddleQuestions: TextView
+    fun butAnswer(view: View){
+        var text = intent.getStringExtra("Result").toString()
+        answer.text = text
+    }
+
     fun clickAtRiddle(view: View){
-        val riddles = listOf("Речка спятила с ума - По домам пошла сама.","Музыкант, певец, рассказчик — А всего труба да ящик.","На раскрашенных страницах Много праздников хранится.",
-            "Ах, не трогайте меня: Обожгу и без огня!","Маленькие домики по улице бегут, Мальчиков и девочек домики везут.",
-            "Пустые отдыхаем, А полные шагаем.","Нет ушей, а слышит. Нету рук, а пишет.","Над нами кверху ногами.","Деревянный брусок, А на нем растет лесок.",
-            "В лесу без огня котел кипит.","На ямке, ямке сто ямок с ямкой.","Кто над нами вверх ногами?","В углу сито, не руками вито.",
-            "Было 4 овцы как","Что это такое: синий, большой, с усами и набит зайцами?")
-            riddleQuestions.text = riddles.random()
+        riddleQuestions.text = riddles.random()
         riddle.isEnabled = false
+        activ.isEnabled = true
     }
     fun mainAct(view: View) {
         val intent = Intent(this, Antwort::class.java)
         startActivity(intent)
-        riddle.isEnabled = true
     }
 
 
